6941572a7a004e57413e676b12bf4a11d3b3acf5
Kiran Machhewar <smachhewar@gmail.com>
2018-04-22 09:15:37 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+//Create the component
+const App = ()=>{
+    return <div>hello World</div>
+}
+//Add the component on the dom
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App/>,document.querySelector('.container'));
